c1ff6cd4b230d37a917ff0973f17e7afd56d531b
Vinicius Goncalo <goncalovinicius15@gmail.com>
2026-06-14 18:29:31 -0300

    Initial commit

---
+++ src/main/java/com/fatec/copahub/CopahubApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.copahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopahubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/copahub/CopahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.copahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
